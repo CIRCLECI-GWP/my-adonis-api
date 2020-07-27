9cba68800ae9890d70f9a5441043f599927afcf8
Ndagi Stanley <ndagis@gmail.com>
2020-07-28 02:09:54 +0300

    Registering routes

--- start/routes.js
+++ start/routes.js
@@ -1,21 +1,14 @@
-'use strict'
+"use strict";
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URLs and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.1/routing
-|
-*/
+const Route = use("Route");
 
-/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use('Route')
+Route.get("/", () => {
+  return { greeting: "Welcome to the Adonis API tutorial" };
+});
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-})
+//User routes
+Route.group(() => {
+  Route.post("create", "UserController.create");
+
+  Route.route("get", "UserController.fetch", ["GET", "POST"]);
+}).prefix("user");
